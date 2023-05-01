02156b22e62e53498302577e766318d35eafae29
User <alexyakun@gmail.com>
2023-05-01 21:48:56 +0300

    Initial commit

---
+++ src/main/java/ru/mpei/dbkafka/DbkafkaApplication.java
@@ -0,0 +1,13 @@
+package ru.mpei.dbkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbkafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbkafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mpei/dbkafka/DbkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mpei.dbkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
